62ac486c40cea1beecb5ab09e51344378f4acc2e
maciasj <jmaciasrojas@gmail.com>
2025-05-14 22:37:44 +0200

    Improve courseInfo Part 2

--- APP/src/App.jsx
+++ APP/src/App.jsx
@@ -1,67 +1,68 @@
-import { useState } from 'react'
 
-const App = () => {
-  const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-    'The only way to go fast, is to go well.'
-  ]
-  const [votes, setVotes] = useState([0,0,0,0,0,0,0,0])
-
-  const [selected, setSelected] = useState(0)
-
-  const Randomizer = () => {
-    return (
-      <>
-        <button onClick={() => setSelected(Math.floor(Math.random() * anecdotes.length))}>
-          next anecdote
-        </button>
-      </>
-    )
-  }
-  const Vote = () => {
-    const handleVote = () => {
-      const newVotes = [...votes]
-      newVotes[selected] += 1
-      setVotes(newVotes)
-    }
-    return (
-      <>
-        <button onClick={handleVote}>
-          vote
-        </button>
-      </>
-    )
-  }
-  const Display = ({anecdote,votes}) =>{
-    return(
-    <>
-      <p>{anecdote}</p>
-      <p> has {votes} votes</p>
-    </>)
-  }
-  const findMax = () =>{
-    let max = 0;
-    for(let i = 1; i<votes.length; i++){
-      if(votes[max]<votes[i]) max = i;
-    }
-    return max;
-  }
-
- 
+import Course from "./components/Course"
+const GroupOfCourses = ({ courses }) => {
   return (
     <div>
-      <Display anecdote={anecdotes[selected]} votes={votes[selected]}/>
-      <Vote/><Randomizer/>
-      <h2>Anecdote with most votes</h2>
-      <Display anecdote={anecdotes[findMax()]} votes={votes[findMax()]}/>
+      {
+        courses.map(course => 
+          <Course key={course.id} course={course} />
+      )}
     </div>
   )
+}
+
+const App = () => {
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+
+        }
+      ]
+    }
+  ]
+
+  return (
+  <div>
+    <GroupOfCourses courses={courses} />
+  </div>
+) 
 }
 
 export default App
